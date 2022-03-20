41567c3074c7d083576d5ffad41cdac79291c861
Marco Link <marco.link@contentful.com>
2022-03-20 20:18:39 +0100

    fix: linting errors

--- packages/cli/src/flags/base.ts
+++ packages/cli/src/flags/base.ts
@@ -4,5 +4,5 @@ export const base = {
   help: flags.help({char: 'h'}),
   token: flags.string({char: 't', description: 'api (query) token', env: 'REDASH_TOKEN'}),
   hostname: flags.string({char: 'n', description: 'redash hostname', default: 'https://redash.io', env: 'REDASH_HOST'}),
-  json: flags.boolean({ description: 'only output JSON response', default: false}),
+  json: flags.boolean({description: 'only output JSON response', default: false}),
 }
